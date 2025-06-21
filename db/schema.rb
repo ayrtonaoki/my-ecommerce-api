2add7739479d3e567b430015e9c50f4997c18c24
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-06-20 22:47:30 -0300

    Creates Category model and validation test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_17_202524) do
+ActiveRecord::Schema.define(version: 2025_06_21_013744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
