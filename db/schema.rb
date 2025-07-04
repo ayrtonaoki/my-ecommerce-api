f2729aa7c1e390b9a494ec22758809d816de0bfe
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-04 17:02:30 -0300

    Add featured attribute to Product model

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,18 @@
-ActiveRecord::Schema.define(version: 2025_06_28_034408) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2025_07_04_194535) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -65,6 +79,7 @@ ActiveRecord::Schema.define(version: 2025_06_28_034408) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status"
+    t.boolean "featured", default: false
     t.index ["productable_type", "productable_id"], name: "index_products_on_productable_type_and_productable_id"
   end
 
