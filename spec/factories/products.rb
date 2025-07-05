ce30f8801ac34f903f4e9de41328ac889fda832d
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-05 17:26:53 -0300

    Fix Product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :product do
-    sequence(:name) { |n| "Game #{n}" }
+    sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 100.0..400.0) }
     image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
@@ -8,7 +8,7 @@ FactoryBot.define do
     featured { true }
 
     after :build do |product|
-      product.productable = create(:game)
+      product.productable ||= create(:game)
     end
   end
 end
