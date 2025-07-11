65955d4f18173c2a81807de6b4be740be670291a
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-10 21:59:27 -0300

    Update UnauthenticatedRequest tests with Product and WishItem relation

--- spec/requests/storefront/v1/products/unauthenticated_requests_spec.rb
+++ spec/requests/storefront/v1/products/unauthenticated_requests_spec.rb
@@ -8,19 +8,23 @@ RSpec.describe 'Storefront V1 Home', type: :request do
     context 'without any params' do
       it 'returns 10 records' do
         get url, headers: unauthenticated_header
+
         expect(body_json['products'].count).to eq 10
       end
 
       it 'returns Products with :productable following default pagination' do
         get url, headers: unauthenticated_header
+
         expected_return = general_products[0..9].map do |product|
           build_game_product_json(product)
         end
+
         expect(body_json['products']).to eq expected_return
       end
 
       it 'returns success status' do
         get url, headers: unauthenticated_header
+
         expect(response).to have_http_status(:ok)
       end
 
@@ -32,12 +36,14 @@ RSpec.describe 'Storefront V1 Home', type: :request do
     context 'with search param' do
       let!(:search_products) do
         products = []
+
         3.times { |n| products << create(:product, name: "Search #{n + 1}") }
         3.times { |n| products << create(:product, description: "Some kind of search #{n + 1}") }
         3.times do |n|
           game = create(:game, developer: "Search #{n + 1}")
           products << create(:product, productable: game)
         end
+
         products
       end
 
@@ -45,22 +51,27 @@ RSpec.describe 'Storefront V1 Home', type: :request do
 
       it 'returns only seached products limited by default pagination' do
         get url, headers: unauthenticated_header, params: search_params
+
         expected_return = search_products[0..8].map do |product|
           build_game_product_json(product)
         end
+
         expect(body_json['products']).to contain_exactly *expected_return
       end
 
       it 'does not return any product out of search' do
         get url, headers: unauthenticated_header, params: search_params
+
         unexpected_return = general_products.map do |product|
           build_game_product_json(product)
         end
+
         expect(body_json['products']).to_not include *unexpected_return
       end
 
       it 'returns success status' do
         get url, headers: unauthenticated_header, params: search_params
+
         expect(response).to have_http_status(:ok)
       end
 
@@ -72,27 +83,31 @@ RSpec.describe 'Storefront V1 Home', type: :request do
     context 'with categories filter' do
       let!(:categories) { create_list(:category, 3) }
       let!(:categories_products) { create_list(:product, 15, category_ids: categories.map(&:id).sample(2)) }
-
       let(:search_params) { { category_ids: categories.map(&:id) } }
 
       it 'returns only search ategorized products limited by default pagination' do
         get url, headers: unauthenticated_header, params: search_params
+
         expected_return = categories_products[0..9].map do |product|
           build_game_product_json(product)
         end
+
         expect(body_json['products']).to contain_exactly *expected_return
       end
 
       it 'does not return any product out of search' do
         get url, headers: unauthenticated_header, params: search_params
+
         unexpected_return = general_products.map do |product|
           build_game_product_json(product)
         end
+
         expect(body_json['products']).to_not include *unexpected_return
       end
 
       it 'returns success status' do
         get url, headers: unauthenticated_header, params: search_params
+
         expect(response).to have_http_status(:ok)
       end
 
@@ -119,22 +134,27 @@ RSpec.describe 'Storefront V1 Home', type: :request do
 
         it 'returns only products with minimal prices limited by default pagination' do
           get url, headers: unauthenticated_header, params: search_params
+
           expected_return = (general_products + higher_price_products)[0..9].map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to contain_exactly *expected_return
         end
 
         it 'does not return any product out of search' do
           get url, headers: unauthenticated_header, params: search_params
+
           unexpected_return = lower_prices_products.map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to_not include *unexpected_return
         end
 
         it 'returns success status' do
           get url, headers: unauthenticated_header, params: search_params
+
           expect(response).to have_http_status(:ok)
         end
 
@@ -148,22 +168,27 @@ RSpec.describe 'Storefront V1 Home', type: :request do
 
         it 'returns only products with maximum prices limited by default pagination' do
           get url, headers: unauthenticated_header, params: search_params
+
           expected_return = lower_prices_products.map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to contain_exactly *expected_return
         end
 
         it 'does not return any product out of search' do
           get url, headers: unauthenticated_header, params: search_params
+
           unexpected_return = (general_products + higher_price_products).map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to_not include *unexpected_return
         end
 
         it 'returns success status' do
           get url, headers: unauthenticated_header, params: search_params
+
           expect(response).to have_http_status(:ok)
         end
 
@@ -177,22 +202,27 @@ RSpec.describe 'Storefront V1 Home', type: :request do
 
         it 'returns only products in price range limited by default pagination' do
           get url, headers: unauthenticated_header, params: search_params
+
           expected_return = (lower_prices_products + higher_price_products).map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to contain_exactly *expected_return
         end
 
         it 'does not return any product out of search' do
           get url, headers: unauthenticated_header, params: search_params
+
           unexpected_return = general_products.map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to_not include *unexpected_return
         end
 
         it 'returns success status' do
           get url, headers: unauthenticated_header, params: search_params
+
           expect(response).to have_http_status(:ok)
         end
 
@@ -205,19 +235,23 @@ RSpec.describe 'Storefront V1 Home', type: :request do
     context 'with release_date filter' do
       let!(:recently_released_products) do
         products = []
+
         5.times do |n|
           game = create(:game, release_date: (0..7).to_a.sample.days.ago)
           products << create(:product, productable: game)
         end
+
         products
       end
 
       let!(:older_products) do
         products = []
+
         5.times do |n|
           game = create(:game, release_date: (8..14).to_a.sample.days.ago)
           products << create(:product, productable: game)
         end
+
         products
       end
 
@@ -226,22 +260,27 @@ RSpec.describe 'Storefront V1 Home', type: :request do
 
         it 'returns only products with minimal released date limited by default pagination' do
           get url, headers: unauthenticated_header, params: search_params
+
           expected_return = recently_released_products.map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to match_array expected_return
         end
 
         it 'does not return any product out of search' do
           get url, headers: unauthenticated_header, params: search_params
+
           unexpected_return = (older_products + general_products).map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to_not include *unexpected_return
         end
 
         it 'returns success status' do
           get url, headers: unauthenticated_header, params: search_params
+
           expect(response).to have_http_status(:ok)
         end
 
@@ -255,22 +294,27 @@ RSpec.describe 'Storefront V1 Home', type: :request do
 
         it 'returns only products with maximum release date limited by default pagination' do
           get url, headers: unauthenticated_header, params: search_params
+
           expected_return = (general_products + older_products)[0..9].map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to contain_exactly *expected_return
         end
 
         it 'does not return any product out of search' do
           get url, headers: unauthenticated_header, params: search_params
+
           unexpected_return = recently_released_products.map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to_not include *unexpected_return
         end
 
         it 'returns success status' do
           get url, headers: unauthenticated_header, params: search_params
+
           expect(response).to have_http_status(:ok)
         end
 
@@ -284,22 +328,27 @@ RSpec.describe 'Storefront V1 Home', type: :request do
 
         it 'returns only products in release date range limited by default pagination' do
           get url, headers: unauthenticated_header, params: search_params
+
           expected_return = (recently_released_products + older_products).map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to contain_exactly *expected_return
         end
 
         it 'does not return any product out of search' do
           get url, headers: unauthenticated_header, params: search_params
+
           unexpected_return = general_products.map do |product|
             build_game_product_json(product)
           end
+
           expect(body_json['products']).to_not include *unexpected_return
         end
 
         it 'returns success status' do
           get url, headers: unauthenticated_header, params: search_params
+
           expect(response).to have_http_status(:ok)
         end
 
@@ -312,24 +361,27 @@ RSpec.describe 'Storefront V1 Home', type: :request do
     context 'with pagination params' do
       let(:page) { 2 }
       let(:length) { 5 }
-
       let(:pagination_params) { { page: page, length: length } }
 
       it 'returns records sized by :length' do
         get url, headers: unauthenticated_header, params: pagination_params
+
         expect(body_json['products'].count).to eq length
       end
 
       it 'returns products limited by pagination' do
         get url, headers: unauthenticated_header, params: pagination_params
+
         expected_return = general_products[5..9].map do |product|
           build_game_product_json(product)
         end
+
         expect(body_json['products']).to contain_exactly *expected_return
       end
 
       it 'returns success status' do
         get url, headers: unauthenticated_header, params: pagination_params
+
         expect(response).to have_http_status(:ok)
       end
 
@@ -339,24 +391,57 @@ RSpec.describe 'Storefront V1 Home', type: :request do
     end
 
     context 'with order params' do
-      let(:order_params) { { order: { name: 'desc' } } }
+      context 'by price' do
+        let(:order_params) { { order: { price: 'desc' } } }
 
-      it 'returns ordered products limited by default pagination' do
-        get url, headers: unauthenticated_header, params: order_params
-        general_products.sort! { |a, b| b[:name] <=> a[:name] }
-        expected_return = general_products[0..9].map do |product|
-          build_game_product_json(product)
+        it 'returns ordered products limited by default pagination' do
+          get url, headers: unauthenticated_header, params: order_params
+
+          general_products.sort! { |a, b| b[:price] <=> a[:price] }
+
+          expected_return = general_products[0..9].map do |product|
+            build_game_product_json(product)
+          end
+
+          expect(body_json['products']).to contain_exactly *expected_return
         end
-        expect(body_json['products']).to contain_exactly *expected_return
+
+        it 'returns success status' do
+          get url, headers: unauthenticated_header, params: order_params
+
+          expect(response).to have_http_status(:ok)
+        end
+
+        it_behaves_like 'pagination meta attributes', { page: 1, length: 10, total: 15, total_pages: 2 } do
+          before { get url, headers: unauthenticated_header, params: order_params }
+        end
       end
 
-      it 'returns success status' do
-        get url, headers: unauthenticated_header, params: order_params
-        expect(response).to have_http_status(:ok)
-      end
+      context 'by release date' do
+        let(:order_params) { { order: { release_date: 'desc' } } }
 
-      it_behaves_like 'pagination meta attributes', { page: 1, length: 10, total: 15, total_pages: 2 } do
-        before { get url, headers: unauthenticated_header, params: order_params }
+        it 'returns ordered products limited by default pagination' do
+          get url, headers: unauthenticated_header, params: order_params
+
+          general_products.sort! { |a, b| b.productable.release_date <=> a.productable.release_date }
+
+          expected_return = general_products[0..9].map do |product|
+            build_game_product_json(product)
+          end
+
+          expect(body_json['products']).to eq(expected_return)
+        end
+
+
+        it 'returns success status' do
+          get url, headers: unauthenticated_header, params: order_params
+
+          expect(response).to have_http_status(:ok)
+        end
+
+        it_behaves_like 'pagination meta attributes', { page: 1, length: 10, total: 15, total_pages: 2 } do
+          before { get url, headers: unauthenticated_header, params: order_params }
+        end
       end
     end
   end
@@ -367,13 +452,24 @@ RSpec.describe 'Storefront V1 Home', type: :request do
 
     it 'returns requested Product' do
       get url, headers: unauthenticated_header
+
       expected_product = build_detailed_game_product_json(product)
+
       expect(body_json['product']).to eq expected_product
     end
 
     it 'returns success status' do
       get url, headers: unauthenticated_header
+
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns right count this products was favorired' do
+      create_list(:wish_item, 5, product: product)
+
+      get url, headers: unauthenticated_header
+
+      expect(body_json['product']['favorited_count']).to eq 5
     end
   end
 
@@ -394,7 +490,7 @@ RSpec.describe 'Storefront V1 Home', type: :request do
     json['categories'] = product.categories.as_json
     json.merge! product.productable.as_json(only: %i(mode release_date developer))
     json['system_requirement'] = product.productable.system_requirement.as_json
-    json['favorited_count'] = 0
+    json['favorited_count'] = product.wish_items.count
     json['sells_count'] = 0
     json
   end
