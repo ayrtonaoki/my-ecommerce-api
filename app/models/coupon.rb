11987d6aa8bcc27eb875235a37a54553bce58aaf
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-12 03:55:57 -0300

    Adds Coupon attributes validations

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,10 +1,20 @@
 class Coupon < ApplicationRecord
-  validates :code, presence: true, uniqueness: { case_sensitive: false  }
+  class InvalidUse < StandardError; end
+
+  include LikeSearchable
+  include Paginatable
+  include NameSearchable
+
+  validates :name, presence: true
+  validates :code, presence: true, uniqueness: { case_sensitive: false }
   validates :status, presence: true
   validates :discount_value, presence: true, numericality: { greater_than: 0 }
-  validates :due_date, presence: true, presence: true, future_date: true
+  validates :due_date, presence: true, future_date: true
 
-  enum status: { active: 1, inactive: 2 }
+  enum status: { active:1,  inactive: 2 }
 
-  include Paginatable
+  def validate_use!
+    raise InvalidUse unless self.active? && self.due_date >= Time.now
+    true
+  end
 end
